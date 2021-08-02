2cac281e37966488cda1f00ce1a041c6c09a406a
Benjamin Krieger <bkrieger@igus.net>
2021-08-02 20:06:41 +0200

    commit of first release

--- src/RabbitMQ.Library/Models/QueueCreateArgument.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RabbitMQ.Library.Models
-{
-    public class QueueCreateArgument
-    {
-        public string Key { get; set; }
-        public string Value { get; set; }
-        public string Type { get; set; }
-    }
-}
