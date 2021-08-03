ee17d3aa04baf3c5ae5ccfea971e4652a5d69966
Benjamin Krieger <bkrieger@igus.net>
2021-08-03 08:26:23 +0200

    Added comment in install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,7 @@
+# ATTENTION: This file is not for direct installation purposes. 
+# It's used to pack an archive with a published single-file app 
+# and optimize the installation of a release
+
 $programPath = "$env:LOCALAPPDATA\RabbitCLI"
 New-Item -Path $programPath -ItemType Directory -Force
 Copy-Item -Path "rabbitcli.exe" "$programPath\rabbitcli.exe"
