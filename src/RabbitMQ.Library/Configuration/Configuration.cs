27b3255ebed825dc45303d1cb58873cfbbb4fd94
Benjamin Krieger <bkrieger@igus.net>
2021-08-06 20:27:07 +0200

    Extended configuration to also contain simple properties instead only a list of rabbitmq configs. Added migration from old format to make it downward compatible

---
+++ src/RabbitMQ.Library/Configuration/Configuration.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RabbitMQ.Library.Configuration
+{
+    public class Configuration
+    {
+        public string TextEditorPath { get; set; } = "notepad";
+        public Dictionary<string, RabbitMqConfiguration> ConfigurationCollection { get; set; }
+    }
+}
--- src/RabbitMQ.Library/Configuration/ConfigurationManager.cs
+++ src/RabbitMQ.Library/Configuration/ConfigurationManager.cs
@@ -2,98 +2,134 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Newtonsoft.Json;
 
 namespace RabbitMQ.Library.Configuration
 {
     public class ConfigurationManager
     {
-        protected Dictionary<string, RabbitMqConfiguration> ConfigurationCollection { get; set; }
+        private Configuration _config;
         private string _encryptionKey;
 
         public ConfigurationManager()
         {
             //_encryptionKey = Environment.MachineName + Environment.UserName + "!#RabbitMqCli#!";
             _encryptionKey = "!#RabbitMqCli#!";
+        }
 
+        public void Initialize()
+        {
             EnsureConfigFileExists();
-
             LoadConfiguration();
         }
 
         public bool Exists(string name)
         {
-            return ConfigurationCollection.ContainsKey(name);
+            return _config.ConfigurationCollection.ContainsKey(name);
+        }
+
+        public void SetProperty(string property, string value)
+        {
+            var prop = typeof(Configuration).GetProperty(property,
+                BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Public);
+            if (prop == null)
+            {
+                throw new Exception($"No property found with name '{property}'");
+            }
+            prop.SetValue(_config, value);
+
+            SaveConfiguration();
+        }
+
+        public string GetProperty(string property)
+        {
+            var prop = typeof(Configuration).GetProperty(property,
+                BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Public);
+            if (prop == null)
+            {
+                throw new Exception($"No property found with name '{property}'");
+            }
+            return (string)prop.GetValue(_config);
         }
 
         public RabbitMqConfiguration Get(string name)
         {
-            if (!ConfigurationCollection.ContainsKey(name))
+            if (!_config.ConfigurationCollection.ContainsKey(name))
             {
                 throw new Exception($"Configuration with the name \"{name}\" does not exists.");
             }
 
-            return ConfigurationCollection[name];
+            return _config.ConfigurationCollection[name];
         }
 
         public void AddConfiguration(RabbitMqConfiguration config)
         {
-            if (ConfigurationCollection.ContainsKey(config.Name))
+            if (_config.ConfigurationCollection.ContainsKey(config.Name))
             {
                 throw new Exception($"Configuration with the name \"{config.Name}\" already exists.");
             }
 
-            ConfigurationCollection.Add(config.Name, config);
+            _config.ConfigurationCollection.Add(config.Name, config);
 
             SaveConfiguration();
         }
 
         public void UpdateConfiguration(RabbitMqConfiguration config)
         {
-            if (!ConfigurationCollection.ContainsKey(config.Name))
+            if (!_config.ConfigurationCollection.ContainsKey(config.Name))
             {
                 throw new Exception($"Configuration with the name \"{config.Name}\" does not exists.");
             }
 
-            ConfigurationCollection[config.Name] = config;
+            _config.ConfigurationCollection[config.Name] = config;
 
             SaveConfiguration();
         }
 
         public void RemoveConfiguration(string name)
         {
-            if (!ConfigurationCollection.ContainsKey(name))
+            if (!_config.ConfigurationCollection.ContainsKey(name))
             {
                 throw new Exception($"Configuration with the name \"{name}\" does not exists.");
             }
 
-            ConfigurationCollection.Remove(name);
+            _config.ConfigurationCollection.Remove(name);
 
             SaveConfiguration();
         }
 
         public string[] GetConfigurationKeys()
         {
-            return ConfigurationCollection.Keys.ToArray();
+            return _config.ConfigurationCollection.Keys.ToArray();
+        }
+
+        protected virtual void WriteConfiguration(string data)
+        {
+            File.WriteAllText(GetConfigFilePath(), data);
+        }
+
+        protected virtual string ReadConfiguration()
+        {
+            return File.ReadAllText(GetConfigFilePath());
         }
 
         private void SaveConfiguration()
         {
-            File.WriteAllText(GetConfigFilePath(), SerializeConfigCollection(ConfigurationCollection));
+            WriteConfiguration(SerializeConfiguration(_config));
         }
 
         private void LoadConfiguration()
         {
-            var json = File.ReadAllText(GetConfigFilePath());
-
-            ConfigurationCollection = DeserializeConfigJson(json);
+            var json = ReadConfiguration();
+            _config = DeserializeConfiguration(json);
         }
 
-        private void EnsureConfigFileExists()
+        protected virtual void EnsureConfigFileExists()
         {
             if (!File.Exists(GetConfigFilePath()))
             {
-                ConfigurationCollection = new Dictionary<string, RabbitMqConfiguration>();
+                _config = new Configuration();
                 SaveConfiguration();
             }
         }
@@ -102,6 +138,16 @@ namespace RabbitMQ.Library.Configuration
         {
             var userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             return Path.Combine(userDir, "rabbitcli.json");
+        }
+
+        private string SerializeConfiguration(Configuration config)
+        {
+            var props = typeof(Configuration).GetProperties();
+            var dict = props.Where(p => p.Name != nameof(config.ConfigurationCollection))
+                .ToDictionary(p => p.Name, p => p.GetValue(config));
+            dict.Add(nameof(config.ConfigurationCollection), SerializeConfigCollection(config.ConfigurationCollection));
+
+            return JsonConvert.SerializeObject(dict);
         }
 
         private string SerializeConfigCollection(Dictionary<string, RabbitMqConfiguration> configCollection)
@@ -117,11 +163,30 @@ namespace RabbitMQ.Library.Configuration
             return JsonConvert.SerializeObject(encryptedConfigs);
         }
 
-        private Dictionary<string, RabbitMqConfiguration> DeserializeConfigJson(string json)
+        private Configuration DeserializeConfiguration(string json)
         {
-            var encryptedConfigs = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            var dict = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            var config = new Configuration();
+            
+            // Compatibility to old configs
+            if (!dict.ContainsKey(nameof(config.ConfigurationCollection)))
+            {
+                config.ConfigurationCollection = DeserializeConfigCollection(dict.ToDictionary(d => d.Key, d => d.Value.ToString()));
+                WriteConfiguration(SerializeConfiguration(config));
+                return config;
+            }
+            var props = typeof(Configuration).GetProperties();
+            props.Where(p => p.Name != nameof(config.ConfigurationCollection)).ToList().ForEach(p => p.SetValue(config, dict[p.Name]));
+            var jobject = dict[nameof(config.ConfigurationCollection)];
+            var encryptedDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(JsonConvert.SerializeObject(jobject));
+            config.ConfigurationCollection = DeserializeConfigCollection(encryptedDict);
+            
+            return config;
+        }
 
-            return encryptedConfigs.Select(kv => 
+        private Dictionary<string, RabbitMqConfiguration> DeserializeConfigCollection(Dictionary<string, string> encryptedCollection)
+        {
+            return encryptedCollection.Select(kv => 
                     new KeyValuePair<string, RabbitMqConfiguration>(
                         kv.Key,
                         JsonConvert.DeserializeObject<RabbitMqConfiguration>(kv.Value.Decrypt(_encryptionKey))
